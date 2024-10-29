0efd510dc6f9ac54404216315ca9b86c826facdc
Annylory LR <annyloryrosa@gmail.com>
2024-10-29 17:34:55 -0300

    changing user name and avatar

--- src/User.js
+++ src/User.js
@@ -1,13 +1,38 @@
+import React from "react";
+
+
 export default function User(){
+  let initialName = "catanacomics";
+  const [name, SetName] = React.useState(initialName);
+  let initialAvatar = "assets/img/catanacomics.svg";
+  const [avatar,SetAvatar] = React.useState(initialAvatar);
+
+  function ChangeName(){
+    let nameInput = prompt("Por favor, insira um nome")
+    SetName(nameInput);
+    
+  }
+
+  function ChangeAvatar(){
+    let avatarInput = prompt("Por favor, insira um link para a imagem")
+
+    SetAvatar(avatarInput);
+  }
+
+  let finalName = !name ? initialName : name;
+  let finalAvatar = !avatar ? initialAvatar : avatar;
+
     return(
       <div className="usuario">
-        <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+        <img onClick={ChangeAvatar} src={finalAvatar} alt="imagem de perfil"/>
         <div className="texto">
           <span>
-            <strong>catanacomics</strong>
-            <ion-icon name="pencil"></ion-icon>
+            <strong>{finalName}</strong>
+            <ion-icon onClick={ChangeName} name="pencil"></ion-icon>
           </span>
         </div>
       </div>
     )
 }
+
+
