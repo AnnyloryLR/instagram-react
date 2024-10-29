93c2ae340fdff3fe426c997d8fac1e9254f2308f
Annylory LR <annyloryrosa@gmail.com>
2024-10-29 12:38:06 -0300

    using map for render

--- src/Stories.js
+++ src/Stories.js
@@ -1,81 +1,40 @@
 export default function Stories(){
+  let stories = [{image:"assets/img/9gag.svg", alternative:"9gag" , text:"9gag"},
+    {image:"assets/img/meowed.svg", alternative:"meowed", text:"meowed"},
+    {image:"assets/img/barked.svg", alternative:"barked", text:"barked"},
+    {image:"assets/img/nathanwpylestrangeplanet.svg", alternative:"nathanwpylestrangeplanet", text:"nathanwpylestrangeplanet"},
+    {image:"assets/img/wawawicomics.svg", alternative:"wawawicomics", text:"wawawicomics"},
+    {image:"assets/img/respondeai.svg", alternative:"respondeai", text:"respondeai"},
+    {image:"assets/img/filomoderna.svg", alternative:"filomoderna", text:"filomoderna"},
+    {image:"assets/img/memeriagourmet.svg" ,alternative:"memeriagourmet", text:"memeriagourmet"}
+
+]
     return(
         <div class="stories">
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/9gag.svg" alt="9gag"/>
-              </div>
-              <div class="usuario">
-                9gag
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/meowed.svg" alt="meowed"/>
-              </div>
-              <div class="usuario">
-                meowed
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/barked.svg" alt="barked"/>
-              </div>
-              <div class="usuario">
-                barked
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/nathanwpylestrangeplanet.svg" alt="nathanwpylestrangeplanet"/>
-              </div>
-              <div class="usuario">
-                nathanwpylestrangeplanet
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/wawawicomics.svg" alt="wawawicomics"/>
-              </div>
-              <div class="usuario">
-                wawawicomics
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/respondeai.svg" alt="respondeai"/>
-              </div>
-              <div class="usuario">
-                respondeai
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/filomoderna.svg" alt="filomoderna"/>
-              </div>
-              <div class="usuario">
-                filomoderna
-              </div>
-            </div>
-  
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/memeriagourmet.svg" alt="memeriagourmet"/>
-              </div>
-              <div class="usuario">
-                memeriagourmet
-              </div>
-            </div>
-  
+            { stories.map( (item,i) => <Story image={item.image} alternative ={item.alternative} text ={item.text} key={i} />)}
             <div class="setinha">
               <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
         </div> 
     )
 }
+
+
+function Story({image, alternative, text}){
+  return(
+    <div class="story">
+      <div class="imagem">
+        <img src={image} alt={alternative}/>
+      </div>
+      <div class="usuario">
+         {text}
+      </div>
+    </div>
+
+
+  )
+
+}
+
+
+
